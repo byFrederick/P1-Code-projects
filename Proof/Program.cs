732d484daf223775654588b10292febd41268ac3
byFrederick <freddycruz5227@gmail.com>
2021-05-05 22:59:32 -0400

    Add project Proof

---
+++ Proof/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
