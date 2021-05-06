6f4aeb79fdc26ff85448b8bfa8c3a5bcdfb23e4d
sniperwolf27 <dayanpr2728@gmail.com>
2021-05-05 23:03:13 -0400

    Update to proof

--- Proof/Program.cs
+++ Proof/Program.cs
@@ -6,7 +6,7 @@ namespace Proof
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Le dimo to");
         }
     }
 }
