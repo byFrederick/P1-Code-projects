49df6df495f751e9df48e1e67ad49badd083b76f
byFrederick <freddycruz5227@gmail.com>
2021-05-26 14:31:35 -0400

    Trabajo

---
+++ Tarea 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class convertFaC 
+{
+    public float input;
+    public float output;
+
+    public convertFaC()
+    {
+        Console.WriteLine("Digite la temperatura en grados Farenheit");
+        Console.ReadLine(input);
+        output = (input - 32) * 5 / 9;
+    }
+}
+class program
+{
+    static void Main()
+    {
+        convertFaC convert = new convertFaC();
+        Console.WriteLine("La temperatura en grados celsius es: " + Math.Round(convert.output));
+     }
+}
