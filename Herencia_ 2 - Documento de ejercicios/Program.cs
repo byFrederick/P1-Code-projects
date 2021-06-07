3bb5f6549c27b890a75d5eafd57c6f7be080f306
byFrederick <freddycruz5227@gmail.com>
2021-06-07 15:08:24 -0400

    .

---
+++ Herencia_ 2 - Documento de ejercicios/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class autos
+{
+    public string numSerieMotor;
+    public string marca;
+    public int anno;
+    public float precio;
+    public autos()
+    {
+        Console.WriteLine("Digite el numero de serie del motor");
+        numSerieMotor = Console.ReadLine();
+        Console.WriteLine("Marca del auto");
+        marca = Console.ReadLine();
+        Console.WriteLine("Año del vehiculo");
+        anno = int.Parse(Console.ReadLine());
+        Console.WriteLine("Precio del auto");
+        precio = float.Parse(Console.ReadLine());
+    }
+}
+class autos_vagonetas : autos
+{
+    public string cantPasajeros;
+    public autos_vagonetas()
+    {
+        Console.WriteLine("Digite la cantidad de pasajeros");
+        cantPasajeros = Console.ReadLine();
+    }
+}
+class camionetas : autos
+{
+    public int cantCarga;
+    public int cantEjes;
+    public int rodado;
+    public camionetas()
+    {
+        Console.WriteLine("Digite la capacidad de carga en Kg");
+        cantCarga = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite la cantidad de ejes de su vehiculo");
+        cantEjes = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite el rodado de los neumáticos ");
+        rodado = int.Parse(Console.ReadLine());
+    }
+}
+class program
+{
+    static void Main()
+    {
+        autos_vagonetas auto_vagonetasobj = new autos_vagonetas();
+        Console.WriteLine("");
+        camionetas camionetasobj = new camionetas();
+    }
+}
