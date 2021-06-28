39677d74dc81f753520aa7b8c7ad369784c4e8a9
byFrederick <freddycruz5227@gmail.com>
2021-06-27 22:22:03 -0400

    Parcial 2 y bonos parcialmente completados

---
+++ Bono polimorfismo 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+abstract class Animal
+{
+    public int peso;
+    public Animal()
+    {
+        while(peso < 1)
+        {
+            try
+            {
+                Console.WriteLine("Digite el peso del animal");
+                peso = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Digite un peso válido\n");
+            }
+        }
+    }
+    public virtual void Dice() { }
+}
+class Vaca : Animal
+{
+    public override void Dice()
+    {
+        Console.WriteLine("Muuuuuuu\n\n");
+    }
+}
+class Serpiente : Animal
+{
+    public override void Dice()
+    {
+        Console.WriteLine("ssssssssssss\n\n");
+    }
+}
+class Cerdo : Animal
+{
+    public override void Dice()
+    {
+        Console.WriteLine("oinc oinc\n\n");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Vaca vacaobj = new Vaca();
+        vacaobj.Dice();
+        Console.WriteLine("");
+        Serpiente serpienteobj = new Serpiente();
+        serpienteobj.Dice();
+        Console.WriteLine("");
+        Cerdo cerdobj = new Cerdo();
+        cerdobj.Dice();
+
+    }
+}
--- Parcial2 ejercicio 2/Program.cs
+++ Parcial2 ejercicio 2/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
 
-namespace Parcial2_ejercicio_2
+abstract class Cuadrilatero
 {
-    class Program
+    public float lado1;
+    public float area;
+    public Cuadrilatero()
     {
-        static void Main(string[] args)
+        while(lado1 < 0.01)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Digite el valor del lado 1");
+                lado1 = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Digite un valor válido\n");
+            }
         }
+        area = 0;
+    }
+    public virtual void CalcularArea() { }
+}
+class Cuadrado : Cuadrilatero
+{
+    public override void CalcularArea()
+    {
+        area = lado1 * lado1;
+        Console.WriteLine("El área del cuadrado es " + area);
+    }
+}
+class Rectangulo : Cuadrado
+{
+    public float lado2;
+    public Rectangulo()
+    {
+        while (lado2 < 0.01 || lado1 == lado2)
+        {
+            try
+            {
+                Console.WriteLine("Digite el valor del lado 2 (No puede ser igual al lado 1)");
+                lado2 = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Digite un valor válido\n");
+            }
+        }
+    }
+    public override void CalcularArea()
+    {
+        area = lado1 * lado2;
+        Console.WriteLine("El área del rectángulo es " + area);
+    }
+}
+class Trapecio : Rectangulo
+{
+    public float lado3;
+    public Trapecio(float lado3)
+    {
+        if (lado3 < 0.01 )
+        {
+            {
+                while (this.lado3 < 0.01)
+                {
+                    try
+                    {
+                        Console.WriteLine("Digite el valor de la altura");
+                        this.lado3 = float.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Digite un valor válido\n");
+                    }
+                }
+            }
+        } else
+        {
+            this.lado3 = lado3;
+            Console.WriteLine("El valor de la altura de su trapecio es " + this.lado3);
+        } 
+    }
+    public override void CalcularArea()
+    {
+        area = ((lado1 + lado2) / 2) * lado3;
+        Console.WriteLine("El área del trapecio es " + area);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Cuadrado cuadradobj = new Cuadrado();
+        cuadradobj.CalcularArea();
+        Console.WriteLine("");
+        Rectangulo rectangulobj = new Rectangulo();
+        rectangulobj.CalcularArea();
+        Console.WriteLine("");
+        Trapecio trapeciobj = new Trapecio(0);
+        trapeciobj.CalcularArea();
     }
 }
